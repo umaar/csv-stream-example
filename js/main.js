72e46374fd70804c83f1188d794bed331cb0ad78
Umar Hansa <umar.hansa@gmail.com>
2017-08-09 16:42:52 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,69 @@
+/* global fetch, TextDecoder */
+
+import {TransformStream} from './vendor/transform-stream.js';
+
+class MyTransformer {
+	start() {
+		this.pending = [];
+		this.decoder = new TextDecoder();
+	}
+
+	transform(chunk, controller) {
+		let result = this.decoder.decode(chunk);
+
+		if (this.pending.length > 0) {
+			result = this.pending[0] + result;
+			this.pending = [];
+		}
+
+		const lines = result.split('\n');
+		const characters = result.split('');
+		const isEndingIncomplete = characters[characters.length - 1] !== '\n';
+
+		if (isEndingIncomplete) {
+			this.pending.push(lines.pop());
+		}
+
+		lines.forEach(line => controller.enqueue(line));
+	}
+}
+
+async function init() {
+	const response = await fetch('data/data.csv');
+	const stream = response.body.pipeThrough(new TransformStream(new MyTransformer()));
+	const reader = stream.getReader();
+
+	const allResults = [];
+
+	while (true) {
+		const {value, done} = await reader.read();
+
+		allResults.push(value);
+
+		if (done) {
+			break;
+		}
+	}
+
+	verifyResults(allResults);
+}
+
+function verifyResults(results) {
+	const amount = 9999;
+
+	for (let i = 0; i < amount; i++) {
+		const actualResult = results[i];
+		const expectedResult = `${i},hello world`;
+		const matches = actualResult === expectedResult;
+		console.assert(matches, `Got: '${actualResult}' but needed '${expectedResult}'`);
+
+		if (!matches) {
+			console.info(`Before: ${results[i - 1]}`);
+			console.info(`Current: ${actualResult}`);
+			console.info(`After: ${results[i + 1]}`);
+			break;
+		}
+	}
+}
+
+init();
